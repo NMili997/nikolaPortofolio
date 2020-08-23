4606b26ca8d902ebfa088605076d2979e5ded916
nmili997 <milivojevic97@outlook.com>
2020-08-23 16:58:53 +0200

    Add QRcode in Contact, fix opening links in new tab

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,51 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var message = this.props.data.contactmessage;
     }
 
     return (
       <section id="contact">
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">{message}</p>
+          </div>
+        </div>
+        <div className="two columns header-col">
+          <h3 style={{ color: "white" }}>Scan IT!</h3>
+          <img src="images/qrcode.jpeg" alt="nm_scan"></img>
+        </div>
+        <div className="row">
+          <div className="eight columns"></div>
 
-         <div className="row section-head">
-
-            <div className="two columns header-col">
-
-            
-
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <h4>Address and Phone</h4>
+              <p className="address">
+                {name}
+                <br />
+                {street} <br />
+                {city}, {zip}
+                <br />
+                <span>{phone}</span>
+              </p>
             </div>
-
-            <div className="ten columns">
-
-                  <p className="lead">{message}</p>
-
-            </div>
-
-         </div>
-
-         <div className="row">
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
-            </aside>
-      </div>
-   </section>
+          </aside>
+        </div>
+      </section>
     );
   }
 }
